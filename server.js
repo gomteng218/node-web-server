bc3db355d39465bae0db9615a17f97441215ae27
Pil <gomteng218@gmail.com>
2017-01-08 17:47:13 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var hbs = require('hbs');
+var fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+    var now = new Date().toString();
+    
+    console.log(now + req.method + req.url);
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', function() {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalizeAll', function(text) {
+    return text.toUpperCase();
+});
+
+app.get('/', function(req, res) {
+   res.render('home.hbs', {
+       pageTitle: 'Home Page',
+       currentYear: new Date().getFullYear()
+   });
+});
+
+app.get('/about', function(req, res){
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
+app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
